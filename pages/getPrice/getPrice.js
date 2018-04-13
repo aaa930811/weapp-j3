db5457e754bba0fe5ceca4cdb3e848d5a4973134
aaa930811 <aaa930811@hotmail.com>
2018-04-13 23:08:28 +0800

    修改回退
    Signed-off-by: aaa930811 <aaa930811@hotmail.com>

--- pages/getPrice/getPrice.js
+++ pages/getPrice/getPrice.js
@@ -146,20 +146,21 @@ Page({
         }else{
           wx.showModal({
             title: '提示',
-            content: '没有数据。可能没有报价或关键词错误。关键词列表请看公众号菜单栏~',
-            showCancel:false,
-            confirmText:'确定',
-            // success: function (res) {
-            //   if (res.confirm) {
-            //     console.log('用户点击确定')
-            //     //跳转
-            //     wx.navigateTo({
-            //       url: '../productList/productList'
-            //     })
-            //   } else if (res.cancel) {
-            //     console.log('用户点击取消')
-            //   }
-            // }
+            content: '没有数据。可能没有报价或关键词错误。是否跳转至关键词列表？',
+            // showCancel:false,
+            cancelText:'取消',
+            confirmText:'跳转',
+            success: function (res) {
+              if (res.confirm) {
+                console.log('用户点击确定')
+                //跳转
+                wx.navigateTo({
+                  url: '../productList/productList'
+                })
+              } else if (res.cancel) {
+                console.log('用户点击取消')
+              }
+            }
           })
         }
       }
