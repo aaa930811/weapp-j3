3f0645f318ab3b4780ded0d3d988e618fd354815
shenao <a.shen@eword.cn>
2018-06-25 14:00:20 +0800

    BUG修复
    Signed-off-by: shenao <a.shen@eword.cn>

--- pages/getPrice/getPrice.js
+++ pages/getPrice/getPrice.js
@@ -115,12 +115,12 @@ Page({
   //     date: e.detail.value
   //   })
   // },
-  // radioChange: function (e) {
-  //   console.log('radio发生change事件，携带value值为：', e.detail.value)
-  //   this.setData({
-  //     showCode: e.detail.value
-  //   })
-  // },
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+    this.setData({
+      showCode: e.detail.value
+    })
+  },
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
     var self = this
